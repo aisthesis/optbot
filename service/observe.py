e659e2d539e0d41800d53c37a7b1f2ffe988500d
Marshall Farrier <info@marshallfarrier.com>
2015-03-25 06:20:56 +0000

    Refactor.

--- service/observe.py
+++ service/observe.py
@@ -26,6 +26,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
+import argparse
 from functools import partial
 import sys
 
@@ -56,6 +57,37 @@ def insert(equities, client):
     else:
         logger.info("all values already present")
 
+def remove(equities, client):
+    _db = client[_constants.DB]
+    _active = _db[_constants.ACTIVE]
+    _bulk = _active.initialize_unordered_bulk_op()
+    _values = [{'equity': _eq} for _eq in equities]
+    _alreadydone = True
+    for _val in _values:
+        if _active.find_one(_val) is None:
+            logger.info("{} not present in {}.{}".format(_val, _constants.DB, _constants.ACTIVE))
+        else:
+            _alreadydone = False
+            _bulk.find(_val).remove()
+            logger.info("{} queued for removal from {}.{}".format(_val, _constants.DB, _constants.ACTIVE))
+    if not _alreadydone:
+        try:
+            _result = _bulk.execute()
+        except BulkWriteError:
+            logger.exception("Error writing to database")
+        else:
+            logger.info("{} records removed from {}.{}".format(_result['nRemoved'], _constants.DB, _constants.ACTIVE))
+    else:
+        logger.info("none of given values found")
 
 if __name__ == '__main__':
-    conn.job(partial(insert, sys.argv[1:]), logger)
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument('--add', nargs='+', help='start observing options for given equities')
+    _parser.add_argument('--remove', nargs='+', help='stop observing options for given equities')
+    _argsdict = vars(_parser.parse_args())
+    if _argsdict['add'] is not None:
+        conn.job(partial(insert, _argsdict['add']), logger)
+    if _argsdict['remove'] is not None:
+        conn.job(partial(remove, _argsdict['remove']), logger)
+    if _argsdict['add'] is None and _argsdict['remove'] is None:
+        _parser.print_help()
