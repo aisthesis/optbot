f31c14726927dd099368fa82fc1ccb22ea5911c9
Marshall Farrier <info@marshallfarrier.com>
2015-04-01 01:36:43 +0000

    Documentation for mongo start script.

--- mongo/startmongo.sh
+++ mongo/startmongo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Run as user 'mongodb':
 # $ sudo -u mongodb ./startmongo.sh
+# To do this with no password for sudo, first do:
+# $ sudo su
+# You are then root and can start mongo as user `mongodb`
 mongod --fork --config /etc/mongod.conf
